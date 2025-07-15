4f9997c36434e6c3158baf751e92eb0e652b6841
Jon T <jtoews32@gmail.com>
2025-07-14 22:47:21 -0400

    PhotoWebCam

--- src/PhotoWebCam.js
+++ src/PhotoWebCam.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import Webcam from "react-webcam";
 import { useContext, useRef, useState } from "react";
-
 import { Context } from './GlobalContext';
 
-
 function PhotoWebCam() {
-
-
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
   const { pipeline, setPipeline } = useContext(Context);
@@ -22,42 +18,83 @@ function PhotoWebCam() {
     setPipeline({ ...pipeline, state: 1 });
   };
 
+  const back = (e) => {
+    e.preventDefault();
+    console.log("Going back to webcam...");
+    setPipeline({ ...pipeline, state: pipeline.state - 1 });
+  };
 
+  const uploadPhoto = (e) => {
+    e.preventDefault();
+    console.log("Uploading photo...");
+    setPipeline({ ...pipeline, state: 2 });
+  };
+
+
+  const buttonGrid = {
+     minWidth: "10em",
+     maxWidth: "10em"
+  };
+
+ 
 
   return (
     <div className="container">
- 
       {pipeline.state === 0 && <div>
+        <h2>Capture Photo Using Interface</h2>
         <Webcam height={600} width={600} ref={webcamRef} />
         <br />
-        <button className="primary" onClick={(e) => takePhoto(e)} >Capture Photo</button>
-        <br />
+        <button className="  outline" onClick={(e) => takePhoto(e)} >Capture Photo</button>
+        <br /> <br />
       </div>}
 
       {pipeline.state === 1 && <div>
 
         {imgSrc && (
           <div>
-            <h2>Captured Photo</h2>
-            
+            <h2>Application Verification of Size Requirements</h2>
+
+            <h5><i>Verification Passed</i></h5>
+            <br />
             <img src={imgSrc} alt="Captured" />
 
-            <br />
+            <br /> <br />
 
-            <h3><i>Verification Passed</i></h3>
-            <br />
-            <button className="primary" onClick={(e) => takePhoto(e)} >Upload</button>
-            <br />
+
+            <div className="grid" style={buttonGrid}>
+              <div> <button className="   outline" onClick={(e) => back(e)} >Back</button></div>
+              <div> <button className="   outline" onClick={(e) => uploadPhoto(e)} >Upload</button></div>
+
+            </div>
+
+
+
+            <br /> <br />
 
 
           </div>)}
       </div>}
 
-      {pipeline.state === 2 && <h2> State 2 </h2>}
+      {pipeline.state === 2 && <div>
+        <h2>Upload and Check</h2>
+
+        <span aria-busy="true">Waiting for Upload to Complete</span>
+
+
+      </div>}
+
+
+      {pipeline.state === 3 && <div>
+        <h2>Upload Presents Image</h2>
+
+        <span aria-busy="true">Waiting for Upload to Complete</span>
+
+
+      </div>}
 
 
 
- 
+
     </div>
   );
 };
